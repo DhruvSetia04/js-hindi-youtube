1b9435f77ea3b705540e00679ef250ce3d9be66e
DhruvSetia04 <dhruvsetia729@gmail.com>
2025-10-07 20:31:02 +0000

    controller config

--- basics/part1.js
+++ basics/part1.js
@@ -1,9 +1,18 @@
-console.log("This is a test file.")
-console.log('This is a test file.');
-let name = dhruv 
+let name = 'Dhruv' 
 let number = 5
 const accountid = 23345 
 var rollno = 23 
-city = pilibanga 
+city = 'pilibanga'
+
+
+
+
+var rollno = 22 
+city = 'gangapur' 
+
+
+
+
+
 console.log(name, number, accountid, rollno, city);
 console.table({name, number, accountid, rollno, city});
