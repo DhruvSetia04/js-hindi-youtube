ef7a64a230f7e0382c151d626b33b737b4ca573d
DhruvSetia04 <dhruvsetia729@gmail.com>
2025-10-10 19:57:18 +0000

    config control

---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict ";
+
+// Data types in JavaScript
+
+let name = "dhruv";
+let age = 20;
+let isLoggedIn = false;
+let state;
+let nullVar = null;
+let symbol = Symbol("This is a symbol");
+// BigInt
+let bigInt = 1234567890123456789012345678901234567890n; 
+console.log(typeof "dhruv");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof nullVar);
+console.log(typeof symbol);
+console.log(typeof bigInt);
+console.log(typeof undefined);
+
+//string => ""
+//number => 2, 3.4, -5.6
+//boolean => true/false
+//undefined => variable not assigned variable create but not assigned
+//null => empty value standalone value
+//symbol => unique value
+//BigInt => larger than 2^53 -1
